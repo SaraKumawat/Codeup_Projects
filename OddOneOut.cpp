8c8bf22407c2634b5f75cad2fea072501d3551e5
SaraKumawat <sarakumawat11@gmail.com>
2025-11-06 14:41:52 +0530

    Enhance OddOneOut.cpp with comments for clarity
    Added comments to clarify the functionality of the unique function and its usage in main.

--- OddOneOut.cpp
+++ OddOneOut.cpp
@@ -4,12 +4,12 @@ using namespace std;
 void unique(int nums[], int n)
 {
     unordered_map<int,int>mp;// Map -> (element,frequency)
-    for(int i=0;i<n;i++){
+    for(int i=0;i<n;i++){ // fill the map with frequency of each element
         mp[nums[i]]++;
     }
     for(auto ele : mp){
-        if(ele.second == 1){
-            cout<<ele.first<<endl;
+        if(ele.second == 1){ // if frequency is 1 -> num is unique 
+            cout<<ele.first<<endl; // we have founded our result -> print it
             break;
         }
     }
@@ -20,11 +20,13 @@ int main()
     int n;
     cout << "Enter the size of the array : ";
     cin >> n;
-    int nums[n];
+
+    int nums[n]; // array of size n
+
     for (int i = 0; i < n; i++)
     {
-        cin >> nums[i];
+        cin >> nums[i];  // input array elements
     }
-    unique(nums,n);
+    unique(nums,n); // function calling
     return 0;
 }
